98306b5270847e324abb2223b6a728c26e2f7d7e
Ahmed Shendy <a.shendy@cequens.com>
2016-07-26 16:21:20 +0200

    add env recipe to activemq cookbook

--- activemq/recipes/envvars.rb
+++ activemq/recipes/envvars.rb
@@ -1,14 +1,32 @@
-Chef::Log.info("REDIS_WRITABLE_IP: #{node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_IP]}")
-Chef::Log.info("REDIS_WRITABLE_PORT: #{node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_PORT]}")
+Chef::Log.info("Setting environment variables")
 
-ruby_block  "set-app-var" do
-  block do
-    ENV["REDIS_WRITABLE_IP"] = node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_IP]
-    ENV["REDIS_WRITABLE_PORT"] = node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_PORT]
-  end
+Chef::Log.info("Setting environment variables for current process")
+node[:environment_variables].each do |name, value|
+	ENV["#{name}"] = "#{value}"
 end
 
-execute "export vars" do
-  command "export REDIS_WRITABLE_IP=#{node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_IP]}"
-  command "export REDIS_WRITABLE_PORT=#{node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_PORT]}"
+Chef::Log.info("Writing variables to /etc/environment to have them after restart")
+template "/etc/environment" do
+	source "environment.erb"
+	mode "0644"
+	owner "root"
+	group "root"
+	variables({
+		:environment_variables => node[:environment_variables]
+	})
 end
+
+Chef::Log.info("Creating shell file to export variables")
+template "/usr/local/bin/environment.sh" do
+	source "environment.sh.erb"
+	mode "0755"
+	owner "root"
+	group "root"
+end
+
+Chef::Log.info("Exporting variables for every new created process")
+execute "/usr/local/bin/environment.sh" do
+	user "root"
+	action :run
+end
+
