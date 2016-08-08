468546c3adc30396bb1a90c77ee4ef051058a11a
Ahmed Shendy <a.shendy@cequens.com>
2016-08-08 18:55:03 +0200

    up

--- opsworks_nodejs/metadata.rb
+++ opsworks_nodejs/metadata.rb
@@ -1,2 +1,5 @@
-name "opsworks_nodejs"
-version "0.1.0
+name        "opsworks_nodejs"
+description 'Installs and configures a Node.js application server'
+maintainer  "Cequens"
+license     "Apache 2.0"
+version     "1.0.0"
